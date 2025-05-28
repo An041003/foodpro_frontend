b4aeb8a4c456ff941266e5987f59b905196e8dbf
An041003 <binhan214981@gmail.com>
2025-05-29 00:11:37 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../backend/staticfiles',
-    emptyOutDir: true,
-    assetsDir: '',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    outDir: 'dist', 
   },
 });
